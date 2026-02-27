fa4746c6890e915e8b89c1e3fd7874cdad200711
rizric13 <rizric13@ecolecatholique.ca>
2026-02-27 10:53:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print('Turns out this works, but there is like literly no indentation or AI autocomplete which sucks.')
+print('Now i can edit the file too!')
+input('Do you know a way to run it tho? ')
